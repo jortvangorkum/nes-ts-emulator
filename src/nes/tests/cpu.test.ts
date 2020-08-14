fab2dba7c735836170ac5f32ae64514a169b58b1
Jort van Gorkum <jortvangorkum@gmail.com>
2020-08-14 15:26:48 +0200

    Small changes

--- src/nes/tests/cpu.test.ts
+++ src/nes/tests/cpu.test.ts
@@ -2,11 +2,14 @@ import fs from 'fs';
 import Bus from '../bus';
 import { join } from 'path';
 
+const buffer = fs.readFileSync(join(__dirname, `../../../public/cartridges/adc_1.nes`));
+const nes = new Bus(buffer);
 
-test('Test dummy read', async () => {
-    const buffer = fs.readFileSync(join(__dirname, `../../../public/cartridges/adc_1.nes`));
-    const nes = new Bus(buffer);
+beforeEach(() => {
     nes.cpu.reset();
+})
+
+test('Read from NES file', async () => {
     for (let i = 0; i < 10; i++) {
         nes.clock();
     }
